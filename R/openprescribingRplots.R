fadc551b8926fa841efb2cd32d9b84c986f1c115
Fergus Taylor <fergustay@gmail.com>
2017-08-15 19:45:54 +0100

    tidied up the documentation

--- R/openprescribingRplots.R
+++ R/openprescribingRplots.R
@@ -1,6 +1,6 @@
-#' OpenprescribingRplot: A small package for creating plots in R from openprescribing data.
+#' OpenprescribingRplots: A small package for creating plots in R from openprescribing data.
 #'
-#' @section openprescribingRplot Functions:
+#' @section openprescribingRplots Functions:
 #'
 #' * plot2017(chemical_section_or_presentation_code = NULL)
 #'
